61e9a5e1a620f148aac2b98024bc58b371bdc088
Sayem <afm.sayem@gmail.com>
2017-01-18 01:39:56 +0600

    add google analytics

--- js/landing.js
+++ js/landing.js
@@ -2,6 +2,12 @@ $(document).ready(function() {
   function openSignup() {
     $('.open-dialog').each(function (index, element) {
       element.onclick = function () {
+        ga('send', {
+          hitType: 'event',
+          eventCategory: 'WriteReview',
+          eventAction: 'open',
+          eventLabel: 'Platform E Pop Up Event'
+        });
         $('.modal')[0].classList.add('active');
       };
     });
