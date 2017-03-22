dbe2c87ea5519e97ce640cf7341786e36fb88d59
Sayem <afm.sayem@gmail.com>
2017-03-22 12:41:04 +0600

    revamp

--- js/landing.js
+++ js/landing.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  const base = 'https://website-athena.herokuapp.com';
   function openSignup() {
     $('.open-dialog').each(function (index, element) {
       element.onclick = function () {
@@ -21,9 +22,47 @@ $(document).ready(function() {
     });
   }
 
+  function showFacebookPosts() {
+    fetch(`${base}/facebook`)
+    .then(response => {
+     return response.json();
+    })
+    .then(data => {
+      const elems = data.map(post => {
+        return `<div class="carousel-cell">
+          <img src=${post.image}>
+        </div>`
+      });
+      $('#facebook-carousel').append(elems.join(''));
+      $('#facebook-carousel').flickity({
+        imagesLoaded: true,
+        percentPosition: false
+      });
+    })
+    .catch(err => {
+      // error
+    })
+
+  }
+
+  function showInstagramPosts() {
+    fetch(`${base}/instagram`)
+    .then(response => {
+     return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    })
+    .catch(err => {
+      // error
+    })
+  }
+
   function init() {
     openSignup();
     closeSignup();
+    showFacebookPosts();
+    showInstagramPosts();
   }
   init();
 })
